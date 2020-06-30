f49b416a5a89345eff92c5aa8e4152b4caaf5c6e
Tim Pollandt <pollti@gmx.net>
2020-06-30 21:48:57 +0200

    removed DM in title

---
+++ bp2019/Parallelism.java
@@ -0,0 +1,28 @@
+public class Parallelism {
+    public static final int PI = 3;
+    
+    /**
+     * Calculates the area of a circle.
+     * @param r radius
+     * @return Area of circle with radius r
+     */
+    public static float areaOfCircle(float r) {
+        // TODO implement this
+        return 0;
+    }
+    
+    /**
+     * Calculates the volume of a sphere.
+     * @param r radius
+     * @return Volume of sphere with radius r
+     */
+    public static float volumeOfSphere(float r) {
+        // TODO implement this
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Area of circle (r=4): " + areaOfCircle(4));
+        System.out.println("Volume of sphere (r=2): " + volumeOfSphere(2));
+    }
+}
